e6ecd24244f74a4b3581ff91e1f74a4146342a31
Sukanya <106397577+Gund008@users.noreply.github.com>
2023-09-10 09:25:03 -0700

    Add files via upload

---
+++ Program13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+
+    
+    //      1          2        3
+    for(iCnt = 1;   iCnt <= 5;  iCnt++)
+    {
+
+        printf("Jay Ganesh...\n");  // 4
+    
+    }
+
+}
+
